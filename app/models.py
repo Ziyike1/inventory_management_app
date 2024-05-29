0712d5ec23bea0607a63561238eafc6bcb32ad95
Ziyike1 <tul51428@temple.edu>
2024-05-29 14:04:30 +0800

    finish the database model which contains 3 tables

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    specification = db.Column(db.String(120))
+    unit = db.Column(db.String(20))
+
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
+    date = db.Column(db.Date)
+    supplier = db.Column(db.String(64))
+
+
+class Sale(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
+    date = db.Column(db.Date)
+    destination = db.Column(db.String(64))
