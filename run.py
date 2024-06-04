78c22197b614ee0bb2ca6d033cd8b4d39a2cb636
Ziyike1 <tul51428@temple.edu>
2024-06-04 14:24:31 +0800

    add the default browser page and set it to open automatically

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+import webbrowser
+from threading import Timer
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000")
+
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    app.run(debug=False)
