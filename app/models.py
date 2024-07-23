1728cf0ca121d6a3fefb1148348be27023da99ad
Ziyike1 <tul51428@temple.edu>
2024-07-23 11:15:02 +0800

    new features and update: 1:add the current_stock to show the current number 2:add a new button to show/hide the stock 3:when add new product/purchase/sale, the id and name will be chosen from the list 4:other text change for user's willing

--- app/models.py
+++ app/models.py
@@ -2,26 +2,27 @@ from app import db
 
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(64), index=True)
     specification = db.Column(db.String(120))
     unit = db.Column(db.String(20))
     initial_stock = db.Column(db.Integer, default=0)
+    current_stock = db.Column(db.Integer, default=0)
 
 
 class Purchase(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(64), index=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    product_id = db.Column(db.String(64), db.ForeignKey('product.id'))
     quantity = db.Column(db.Integer)
     date = db.Column(db.Date)
     supplier = db.Column(db.String(64))
 
 
 class Sale(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(64), index=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    product_id = db.Column(db.String(64), db.ForeignKey('product.id'))
     quantity = db.Column(db.Integer)
     date = db.Column(db.Date)
     destination = db.Column(db.String(64))
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import render_template, flash, redirect, url_for, request, current_app
 from app import db
 from app.models import Product, Purchase, Sale
@@ -21,11 +23,11 @@ def product():
         specification = request.form['specification']
         unit = request.form['unit']
         initial_stock = request.form['initial_stock']
-        product = Product(id=id, name=name, specification=specification, unit=unit, initial_stock=initial_stock)
+        product = Product(id=id, name=name, specification=specification, unit=unit, initial_stock=initial_stock, current_stock=initial_stock)
         db.session.add(product)
         db.session.commit()
-        flash('新产品添加成功')
-        return redirect(url_for('index'))
+        flash('新库存添加成功')
+        return redirect(url_for('index', show_inventory=True))
     return render_template('product.html')
 
 
@@ -38,12 +40,17 @@ def purchase():
         date_str = request.form['date']
         date = datetime.strptime(date_str, '%Y-%m-%d').date()
         supplier = request.form['supplier']
-        purchase = Purchase(product_id=product_id, name=name, quantity=quantity, date=date, supplier=supplier)
+        purchase = Purchase(id=str(uuid.uuid4()), product_id=product_id, name=name, quantity=quantity, date=date, supplier=supplier)
         db.session.add(purchase)
+
+        product = Product.query.get(product_id)
+        product.current_stock += int(quantity)
+
         db.session.commit()
-        flash('采购记录添加成功!')
-        return redirect(url_for('index'))
-    return render_template('purchase.html')
+        flash('入库记录添加成功!')
+        return redirect(url_for('index', show_inventory=True))
+    products = Product.query.all()
+    return render_template('purchase.html', products=products)
 
 
 @current_app.route('/sale', methods=['GET', 'POST'])
@@ -55,36 +62,41 @@ def sale():
         date_str = request.form['date']
         date = datetime.strptime(date_str, '%Y-%m-%d').date()
         destination = request.form['destination']
-        sale = Sale(product_id=product_id, name=name, quantity=quantity, date=date, destination=destination)
+        sale = Sale(id=str(uuid.uuid4()), product_id=product_id, name=name, quantity=quantity, date=date, destination=destination)
         db.session.add(sale)
+
+        product = Product.query.get(product_id)
+        product.current_stock -= int(quantity)
+
         db.session.commit()
-        flash('安装记录添加成功')
-        return redirect(url_for('index'))
-    return render_template('sale.html')
+        flash('出库记录添加成功')
+        return redirect(url_for('index',show_inventory=True))
+    available_products = Product.query.filter(Product.initial_stock > 0).all()
+    return render_template('sale.html', available_products=available_products)
 
 
-@current_app.route('/delete_product/<int:product_id>', methods=['POST'])
+@current_app.route('/delete_product/<product_id>', methods=['POST'])
 def delete_product(product_id):
     product = Product.query.get_or_404(product_id)
     db.session.delete(product)
     db.session.commit()
     flash('产品删除成功!')
-    return redirect(url_for('index'))
+    return redirect(url_for('index',show_inventory=True))
 
 
-@current_app.route('/delete_purchase/<int:purchase_id>', methods=['POST'])
+@current_app.route('/delete_purchase/<purchase_id>', methods=['POST'])
 def delete_purchase(purchase_id):
     purchase = Purchase.query.get_or_404(purchase_id)
     db.session.delete(purchase)
     db.session.commit()
-    flash('采购记录删除成功!')
-    return redirect(url_for('index'))
+    flash('入库记录删除成功!')
+    return redirect(url_for('index',show_inventory=True))
 
 
-@current_app.route('/delete_sale/<int:sale_id>', methods=['POST'])
+@current_app.route('/delete_sale/<sale_id>', methods=['POST'])
 def delete_sale(sale_id):
     sale = Sale.query.get_or_404(sale_id)
     db.session.delete(sale)
     db.session.commit()
-    flash('销售记录删除成功!')
-    return redirect(url_for('index'))
+    flash('出库记录删除成功!')
+    return redirect(url_for('index',show_inventory=True))
