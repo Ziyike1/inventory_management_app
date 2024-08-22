8147b56a61451490e5d3b91a693f0bf927c44ccf
Ziyike1 <tul51428@temple.edu>
2024-08-22 10:48:14 +0800

    new features and update: 1:briefly update

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-# from flask_debugtoolbar import DebugToolbarExtension
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,11 +10,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    # app.config['DEBUG_TB_ENABLED'] = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    # app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-
-    # toolbar = DebugToolbarExtension(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
